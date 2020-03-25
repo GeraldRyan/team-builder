545d358d02ad421a34105c523c0e426ef9178fc3
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-24 21:20:31 -0500

    got form substance finished. about to lift state

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,24 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Form = props =>
 {
 
+  const [formValues, setFormValues] = useState({
+    fname: 'John',
+    lname: 'Wick',
+    email: '',
+    role: ''
+  })
+
+  const onInputChange = event =>
+  {
+    // we are going to update app state with the new value of the input
+    const inputThatChanged = event.target.name
+    const newValueForInput = event.target.value
+    setFormValues({
+      ...formValues,
+      [inputThatChanged]: newValueForInput,
+    })
+  }
+
+  const onFormSubmit = event =>{
+    event.preventDefault()
+    const newTeamMember = {
+      fname:formValues.fname,
+      lname:formValues.lname,
+      email:formValues.email,
+      role:formValues.role
+    }
+    console.log('new team member:', newTeamMember)
+    // setTeam([...team, newTeamMember])
+  }
+
   return (
     <div>
 
 
-      <form>
+      <form onSubmit={onFormSubmit}>
         <h1>Hello</h1>
-        <p>Enter your name:</p>
-        <input type="text"/>
-        <p>Enter your email:</p>
-        <input type="text"/>
-        <p>Enter your role:</p>
-        <input type="text"/><br/><br/>
+
+        <label htmlFor="">
+          First Name
+         <input name='fname' type="text" onChange={onInputChange} value={formValues.fname} />
+        </label>
+        <br />
+        <label htmlFor="">
+          Last name
+        <input name='lname' type="text" onChandddddddge={onInputChange} value={formValues.lname} />
+        </label>
+        <br />
+        <label htmlFor="">
+          Email
+          <input name='email' type="text" onChange={onInputChange} value={formValues.email} />
+        </label>
+        <br />
+        <label htmlFor="">
+          Role
+        <input name='role' type="text" onChange={onInputChange} value={formValues.role} /><br /><br />
+
+        </label>
         <input
-        type='submit'
-      />
+          type='submit'
+        />
       </form>
 
 
